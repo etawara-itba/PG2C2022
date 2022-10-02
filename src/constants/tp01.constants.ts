698f234fea5cad75fcbcb9b6663f7c3d67970894
etawara-itba <etawara@itba.edu.ar>
2022-10-02 14:58:02 +0900

    feat: added userInput & printerCamera

--- src/constants/tp01.constants.ts
+++ src/constants/tp01.constants.ts
@@ -1,3 +1,7 @@
+// control related
+export const KEY_CONTROLS_DOLLY_IN = 'KeyO';
+export const KEY_CONTROLS_DOLLY_OUT = 'KeyP';
+
 // ui related
 export const SETTINGS_DEFAULT_SHAPE = 'A1';
 export const SETTINGS_MIN_HEIGHT = 0.1;
@@ -13,17 +17,22 @@ export const GROUND_SIZE = 500;
 export const SKY_RGB = '#C5C5C5';
 export const GROUND_RGB = '#8B7675';
 
-// camera related
-export const CAMERA_FOV = 75;
-export const CENTER_CAMERA_X = 0;
-export const CENTER_CAMERA_Y = 80;
-export const CENTER_CAMERA_Z = 200;
-export const CAMERA_CONTROLS_MIN_DOLLY = 100;
-export const CAMERA_CONTROLS_MAX_DOLLY = 500;
-
 // printer related
 export const PRINTER_X = -100;
 export const PRINTER_Y = 0;
 export const PRINTER_Z = 0;
 export const PRINT_SPEED = 10;
 export const PRINTED_OBJECT_MAX_HEIGHT = 20;
+
+// camera related
+export const CAMERA_FOV = 75;
+export const CENTER_CAMERA_X = 0;
+export const CENTER_CAMERA_Y = 80;
+export const CENTER_CAMERA_Z = 200;
+export const CENTER_CAMERA_CONTROLS_MIN_DOLLY = 100;
+export const CENTER_CAMERA_CONTROLS_MAX_DOLLY = 500;
+export const PRINTER_CAMERA_X = PRINTER_X;
+export const PRINTER_CAMERA_Y = PRINTER_Y + 50;
+export const PRINTER_CAMERA_Z = PRINTER_Z + 2 * PRINTED_OBJECT_MAX_HEIGHT;
+export const PRINTER_CAMERA_CONTROLS_MIN_DOLLY = 50;
+export const PRINTER_CAMERA_CONTROLS_MAX_DOLLY = 200;
